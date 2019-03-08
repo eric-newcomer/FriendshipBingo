af1f4a8fa9671cb093503cc039888ae04d117065
Eric <enewcomer23@gmail.com>
2019-03-08 08:42:38 -0800

    Fix random num generator (now rand from 0-24)

--- FriendshipBingo/FriendshipBingo/Board.xaml.cs
+++ FriendshipBingo/FriendshipBingo/Board.xaml.cs
@@ -7,31 +7,34 @@ namespace FriendshipBingo
 {
     public partial class Board : ContentPage
     {
+        List<int> randList = new List<int>();
+
         public Board(Game game)
         {
             Random rand = new Random();
             foreach (KeyValuePair<string, string> entry in game.FactDict)
             {
-                int tileNum = rand.Next(26);
-                string n = "control";
-                string tile = String.Concat(n, tileNum.ToString());            
-
+                int tileNum = rand.Next(24); // gets random number between 0 and 24
+                randList.Add(tileNum);
+                //string n = "control";
+                //string tile = String.Concat(n, tileNum.ToString());           
             }
             InitializeComponent();
         }
 
         void Handle_Clicked(object sender, System.EventArgs e)
         {
-            //var b = (Button)sender;
-            //if (b.BackgroundColor == Color.Green)
-            //{
-            //    b.BackgroundColor = Color.DarkOrange;
-            //}
-            //else
-            //{
-            //    b.BackgroundColor = Color.Green;
-            //}
-            Navigation.PushAsync(new QuestionPage());
+            var b = (Button)sender;
+
+            if (b.BackgroundColor == Color.Green)
+            {
+                b.BackgroundColor = Color.DarkOrange;
+            }
+            else
+            {
+                b.BackgroundColor = Color.Green;
+            }
+            //Navigation.PushAsync(new QuestionPage());
         }
 
         // ** hide navigation **
